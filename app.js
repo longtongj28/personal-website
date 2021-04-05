131b9da6f286aeab6477acda0f22a959cfcf06a0
longtongj28 <tong.johnson.28@gmail.com>
2021-04-05 00:02:51 -0700

    finished projects page and interests page, added sticky nav

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+// site entrance animation
 const t1 = gsap.timeline({defaults:{duration: 2.5}});
 
 t1.to('.welcome', {opacity:1, duration: 2.5})
 .to('.welcome', {opacity:0}, "+=1")
 .to('.loading', {y: '-100%', duration: 1})
-.fromTo('.nav-btn-list', 1.5, {y: '-150%'}, {y: '0%', ease: Power2.ease})
+.fromTo('.navbar-container', 1.5, {opacity: 0}, {opacity: 1, ease: Power2.ease})
+.fromTo('#navlink1', 1, {y: '-150%'}, {y: '0%', ease: Power2.ease}, "-=1.5")
+.fromTo('#navlink2', 1, {y: '-150%'}, {y: '0%', ease: Power2.ease}, "-=1.25")
+.fromTo('#navlink3', 1, {y: '-150%'}, {y: '0%', ease: Power2.ease}, "-=1.0")
+.fromTo('#navlink4', 1, {y: '-150%'}, {y: '0%', ease: Power2.ease}, "-=0.75")
+.fromTo('#navlink5', 1, {y: '-150%'}, {y: '0%', ease: Power2.ease}, "-=0.6")
 .fromTo('#intro3', 2.5, {x: '-150%'}, {x: '0%', ease:Power2.ease}, "-=1.5")
 .fromTo('#intro2', 2.5, {x: '-150%'}, {x: '0%', ease:Power2.ease}, "-=2.5")
 .fromTo('#intro1', 2, {x: '-150%'}, {x: '0%', ease:Power2.ease}, "-=2")
@@ -12,6 +18,7 @@ t1.to('.welcome', {opacity:1, duration: 2.5})
 .fromTo('.fab.fa-linkedin', 1.25, {x:'250%'}, {x:'0%', ease:Power2.ease}, "-=1.2")
 .to('body',{overflowY: 'scroll', duration: 0})
 
+// resume animation (slide from right)
 const resume_btn = document.getElementById("resume-btn");
 const resume = document.getElementById("resume");
 let resume_opened = false;
@@ -26,3 +33,19 @@ resume_btn.addEventListener("click", () => {
     }
     resume_opened = !resume_opened;
 })
+
+//sticky nav bar
+window.onscroll = () => {
+    let navbar = document.getElementById("navbar");
+    let sticky = navbar.offsetTop;
+    let top_btn = document.getElementById("top-btn");
+
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+        top_btn.classList.add("toppage-sticky")
+    }
+    else {
+        navbar.classList.remove("sticky");
+        top_btn.classList.remove("toppage-sticky");
+    }
+}
